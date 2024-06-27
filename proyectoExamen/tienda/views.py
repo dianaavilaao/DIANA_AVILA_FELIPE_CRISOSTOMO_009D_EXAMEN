0f4aab742a271d59ce472754832049d23286d1bb
dianaavilaao <165206809+dianaavilaao@users.noreply.github.com>
2024-06-26 21:58:53 -0400

    modelos

--- proyectoExamen/tienda/views.py
+++ proyectoExamen/tienda/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Producto
+
 
 def index (request):
     context={}
     return render(request, 'tienda/index.html', context)
+
+
+def listar_productos(request):
+    productos = Producto.objects.all()  # Obtén todos los productos desde la base de datos
+    return render(request, 'index.html', {'productos': productos})
